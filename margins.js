bda65e78553f0c8357819129a4963c6417d6137f
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2023-02-22 21:52:05 -0500

    updates

---
+++ margins.js
@@ -0,0 +1,49 @@
+
+let grouping = document.querySelector(".grouping");
+let marginWidth;
+  let aboutBtn = document.querySelector("#about-btn")
+  let workBtn = document.querySelector("#work-btn")
+  let projectCont = document.querySelector(".dos");
+  let windowHeight;
+
+  function calculatePaddings(){
+    let contentWidth = document.querySelector(".conty").clientWidth;
+    let windowWidth = $(window).innerWidth();
+    var p = document.querySelector(".padding-conty");
+    var pStyle = p.currentStyle || window.getComputedStyle(p);
+    let marginStyle = parseFloat(pStyle["padding-left"], 10);
+    marginWidth = (windowWidth - contentWidth) / 2 + marginStyle;
+  }
+
+  function resizePaddings() {
+    grouping.style.right = `${marginWidth}px`;
+  }
+
+  function resizeFullHeight() {
+    projectCont.style.height = `${windowHeight - 240 - 64}px`
+  }
+
+  function calculateHeights() {
+    windowHeight = $(window).height();
+  }
+
+  calculatePaddings();
+  resizePaddings();
+  calculateHeights();
+  resizeFullHeight();
+
+  window.addEventListener('resize', function () {
+    calculatePaddings();
+    resizePaddings();
+    calculateHeights();
+    resizeFullHeight();
+  });
+
+    aboutBtn.addEventListener("click", function() {
+window.location = "index.html#bio-cont"
+  });
+
+  workBtn.addEventListener("click", function() {
+window.location = "index.html#work-cont"
+  });
+
