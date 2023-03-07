2d41ba19d74f0f9c42d0c4716d1040217f4c8f51
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2023-03-07 17:51:38 -0500

    updates

--- margins.js
+++ margins.js
@@ -5,10 +5,12 @@ let marginWidth;
   let workBtn = document.querySelector("#work-btn")
   let projectCont = document.querySelector(".dos");
   let windowHeight;
+  let windowWidth = $(window).innerWidth();
+ 
 
   function calculatePaddings(){
     let contentWidth = document.querySelector(".conty").clientWidth;
-    let windowWidth = $(window).innerWidth();
+    windowWidth = $(window).innerWidth();
     var p = document.querySelector(".padding-conty");
     var pStyle = p.currentStyle || window.getComputedStyle(p);
     let marginStyle = parseFloat(pStyle["padding-left"], 10);
@@ -46,4 +48,15 @@ window.location = "index.html#bio-cont"
   workBtn.addEventListener("click", function() {
 window.location = "index.html#work-cont"
   });
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    let toggleWidth = document.querySelector(".toggle").clientWidth;
+    let groupingWidth = document.querySelector(".grouping").clientWidth;
+console.log(toggleWidth);
+let groupingRight = windowWidth/2 - toggleWidth/2;
+grouping.style.right = `${groupingRight}px`;
+}
+
+
+
 
